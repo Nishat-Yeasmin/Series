3fb5f2995e0db24909509fc62f230f7547e31a59
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2025-10-04 10:53:45 +0600

    Add files via upload

---
+++ Sum of odd numbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,sum=0;
+    cout<<"Enter the last number: ";
+    cin>>n;
+    for(i=1;i<=n;i+=2)
+    {
+        if(i%2!=0)
+        {
+            sum = sum + i;
+        }
+    }
+    for(i=1;i<=n;i+=2)
+    {
+        if(i==n || i+2>n)
+            cout<<i;
+        else
+            cout<<i<<" + ";
+    }
+    cout<<" = "<<sum<<endl;
+    return 0;
+}
